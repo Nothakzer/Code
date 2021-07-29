30b737bc9e9b632f30c151541773fa5791658385
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2021-07-29 13:03:51 -0700

    Theres the code
    lolol

---
+++ security.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+password = int(input("Password:"))
+print (password)
+if (password) == 12335943:
+    nextpass = int(input("Correct, type the second password: "));
+else:
+    print ("Wrong lol get rektttttt")
+    
+if (nextpass) == 22121232:
+    print ("Correct, access Granted");    
+    print ("Heres the data:")
+    print ("Name: UR MOM \n Age: OLD \n Description: FAT LMAO \n Funny: no..im sorry...")
+else:
+    print ("Lol so close lmao")
+    
